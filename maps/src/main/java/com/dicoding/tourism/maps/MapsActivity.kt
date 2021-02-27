89ce6b96ff0fa7b767490223bdf7884ef7bd6816
arifahmadalfian <arifahmadalfian@gmail.com>
2021-02-27 11:23:08 +0700

    deep link MainActivity to MapsActivity

--- maps/src/main/java/com/dicoding/tourism/maps/MapsActivity.kt
+++ maps/src/main/java/com/dicoding/tourism/maps/MapsActivity.kt
@@ -2,15 +2,42 @@ package com.dicoding.tourism.maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import com.dicoding.tourism.maps.databinding.ActivityMapsBinding
+import com.dicoding.tourismapp.core.data.Resource
 
 class MapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMapsBinding
+    private val mapsViewModel: MapsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = "Tourism Maps"
+        getTourismData()
+    }
+
+    private fun getTourismData() {
+        mapsViewModel.tourism.observe(this, { tourism ->
+            if (tourism != null) {
+                when (tourism) {
+                    is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
+                    is Resource.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.tvMaps.text = "this is map of ${tourism.data?.get(0)?.name}"
+                    }
+                    is Resource.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.tvError.visibility = View.VISIBLE
+                        binding.tvError.text = tourism.message
+                    }
+                }
+
+            }
+        })
     }
 }
--- maps/src/main/java/com/dicoding/tourism/maps/MapsViewModel.kt
+++ maps/src/main/java/com/dicoding/tourism/maps/MapsViewModel.kt
@@ -1,9 +1,10 @@
 package com.dicoding.tourism.maps
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 
-class MapsViewModel(tourismUseCase: TourismUseCase): ViewModel() {
+class MapsViewModel @ViewModelInject constructor(tourismUseCase: TourismUseCase): ViewModel() {
     val tourism = tourismUseCase.getAllTourism().asLiveData()
 }
