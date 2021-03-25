374ffbfd590bcdaeef7169c60b9d3523b9e93f5e
arifahmadalfian <arifahmadalfian@gmail.com>
2021-03-25 20:13:26 +0700

    Enkripsi Database dengan SQLiteCipher

--- core/src/main/java/com/dicoding/tourismapp/core/di/CoreModule.kt
+++ core/src/main/java/com/dicoding/tourismapp/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -21,10 +23,14 @@ val databaseModule = module {
         get<TourismDatabase>().tourismDao()
     }
     single {
+        val passphrese: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrese)
         Room.databaseBuilder(
             androidContext(),
             TourismDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
